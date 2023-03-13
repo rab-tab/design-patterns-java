58aa8c6a335a897e5fff9356a01fef814d4d14e1
Rabia <sdetcode@gmail.com>
2023-03-13 17:04:56 +0530

    chain of responsibility - ATM

---
+++ src/main/java/ChainOfResponsibility/Client.java
@@ -0,0 +1,24 @@
+package ChainOfResponsibility;
+
+public class Client {
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+
+        System.out.println("\n ----------------Requested Amount 475-----------------------\n");
+
+        atm.withdraw(475);
+
+
+        System.out.println("\n ----------------Requested Amount 455-----------------------\n");
+
+        atm.withdraw(455);
+
+
+        System.out.println("\n ----------------Requested Amount 520-----------------------\n");
+
+        atm.withdraw(520);
+
+
+    }
+
+}
